64ee80aa0c538e3c918395e25ab313724fd86a63
MHSourov621 <mhsourov621@gmail.com>
2023-05-17 23:04:03 +0600

    assignment start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-black bg-red-500'>This is my assignment</h1>
+      <button className="btn btn-accent">Button</button>
+    </div>
+  )
+}
+
+export default App
