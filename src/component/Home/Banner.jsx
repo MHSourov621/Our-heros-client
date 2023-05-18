abcfe014e88cf00e3460448f3825f1f07229ef60
MHSourov621 <mhsourov621@gmail.com>
2023-05-18 18:59:41 +0600

    banner resposive

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -10,9 +10,9 @@ const Banner = () => {
                 <div id="slide1" className="carousel-item relative w-full">
                     <img src={banner1} className="w-full" />
                     <div className="absolute h-full rounded-xl flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className="text-white space-y-7 w-1/2 pl-20">
-                            <h2 className="text-6xl font-bold">Justice League Team</h2>
-                            <p>The most popular team in DC. </p>
+                        <div className="text-white space-y-7 md:w-2/3 pl-20">
+                            <h2 className="md:text-6xl md:font-bold">Justice League Team</h2>
+                            <p className='text-[10px]'>The most popular team in DC. </p>
                         </div>
                     </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -23,8 +23,8 @@ const Banner = () => {
                 <div id="slide2" className="carousel-item relative w-full">
                     <img src={banner2} className="w-full" />
                     <div className="absolute h-full rounded-xl flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className="text-white space-y-7 w-1/2 pl-20">
-                            <h2 className="text-6xl font-bold">Power Ranger Dino Charger</h2>
+                        <div className="text-white space-y-7 md:w-2/3 pl-20">
+                            <h2 className="md:text-6xl md:font-bold">Power Ranger Dino Charger</h2>
                         </div>
                     </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
@@ -35,9 +35,9 @@ const Banner = () => {
                 <div id="slide3" className="carousel-item relative w-full">
                     <img src={banner3} className="w-full" />
                     <div className="absolute h-full rounded-xl flex items-center left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-                        <div className="text-white space-y-7 w-1/2 pl-20">
-                            <h2 className="text-6xl font-bold">The Avengers Team</h2>
-                            <p>World most popular series from the marvel indestry.</p>
+                        <div className="text-white space-y-7 md:w-2/3 pl-20">
+                            <h2 className="md:text-6xl md:font-bold">The Avengers Team</h2>
+                            <p className='text-[10px]'>World most popular series from the marvel indestry.</p>
                         </div>
                     </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
