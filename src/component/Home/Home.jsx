e3df4ba2baf6240fcc2ff160b0aea9d1f8980f7b
MHSourov621 <mhsourov621@gmail.com>
2023-05-18 22:08:20 +0600

    woman heros section added

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Banner from './Banner';
 import Gallery from './Gallery';
 import IconicHero from './IconicHero';
+import WomanHeros from './WomanHeros';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
             <IconicHero></IconicHero>
-            {/* <Gallery></Gallery> */}
+            <Gallery></Gallery>
+            <WomanHeros></WomanHeros>
         </>
 
     );
