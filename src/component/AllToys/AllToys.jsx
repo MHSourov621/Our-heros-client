30635a3802cd4328cbe1bedc33529a74eac7c152
MHSourov621 <mhsourov621@gmail.com>
2023-05-20 22:44:52 +0600

    dynamic sort fetch

--- src/component/AllToys/AllToys.jsx
+++ src/component/AllToys/AllToys.jsx
@@ -5,23 +5,31 @@ import AllToyRow from './AllToyRow';
 const AllToys = () => {
     const [search, setSearch] = useState();
     const [heros, sethHeros] = useState([]);
+    const [sort, setSort] = useState(1);
+
 
     useEffect(() => {
-        fetch('https://our-heros-server.vercel.app/products')
+        fetch(`https://our-heros-server.vercel.app/products?value=${sort}`)
             .then(res => res.json())
             .then(data => sethHeros(data))
-    }, [])
+    }, [sort])
 
     const handleSearch = () => {
-        fetch(`https://our-heros-server.vercel.app/searchByProductName/${search}`)
-        .then(res => res.json())
-        .then(data => sethHeros(data))
+        fetch(`https://our-heros-server.vercel.app/searchByProductName/${search}?value=${sort}`)
+            .then(res => res.json())
+            .then(data => sethHeros(data))
     }
     return (
         <div className='mt-8 mb-20 '>
             <div className='flex justify-center mb-8'>
                 <input type="text" onChange={(e) => setSearch(e.target.value)} placeholder="search" className="input input-bordered mr-4" />
-                <button onClick={handleSearch} className='btn bg-orange-400 hover:bg-orange-500'>Search</button>
+                <button onClick={handleSearch} className='btn bg-orange-400 hover:bg-orange-500 mr-10'>Search</button>
+
+                <select onChange={(e) => setSort(parseInt(e.target.value))} name="sort" className="select select-bordered">
+                    <option value={1} disabled selected>Ascending </option>
+                    <option value={-1}>Descending</option>
+                    <option value={1}>Ascending</option>
+                </select>
             </div>
             <div className="overflow-x-auto">
                 <table className="table table-zebra w-full">
